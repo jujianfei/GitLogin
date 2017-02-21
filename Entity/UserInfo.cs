1cdeac482e73298218f6c8c43dc4fbf50464cccd
Micheal <15732625435@163.com>
2017-02-21 10:49:08 +0800

    add a project

---
+++ Entity/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class UserInfo
+    {
+        public static string UserName {get;set;}
+        public static string Pwd { get; set; }
+    }
+}
